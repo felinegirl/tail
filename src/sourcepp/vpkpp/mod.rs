8671a0ddab302ef2d2ef4b84774b42cc78194dcd
loonya <luug@nyacat.moe>
2026-02-08 13:05:38 -0600

    I was the stupid one

--- src/sourcepp/vpkpp/mod.rs
+++ src/sourcepp/vpkpp/mod.rs
@@ -2,25 +2,33 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-use std::ffi::CString;
+use std::{ffi::{CStr, CString, c_char}, str::Utf8Error};
 
 use crate::sourcepp::vpkpp::bindings::vpkpp_pack_file_handle_t;
 
 pub mod bindings;
 
-fn to_stupid_strings(basestring: &str) -> *const i8 {
-    let form = CString::new(basestring).unwrap();
-    form.as_ptr() as *const i8
+//converts cool rust strings to stupid c "strings"
+fn stupid(bwa: &str) -> *const c_char{
+    bwa.as_ptr() as *const c_char
+}
+
+//converts stupid c strings to cool rust strings
+fn cool(bwa: *const c_char) -> Result<&'static str, Utf8Error>{
+    unsafe { CStr::from_ptr(bwa) }.to_str()
 }
 
 pub(crate) fn openvpk(path: &str) -> i32{
     unsafe {
+
+        let path: &str = &format!("{path}\0");
+
         let mut fuckyou: vpkpp_pack_file_handle_t;
-        fuckyou = bindings::vpkpp_open(to_stupid_strings(path), None, None);
+        fuckyou = bindings::vpkpp_open(stupid(path), None, None);
             
-        let awa = bindings::vpkpp_vpk_get_version(fuckyou);
+        let awa = bindings::vpkpp_vpk_get_chunk_size(fuckyou);
         
-        dbg!(fuckyou)
+        dbg!(awa)
         // for a in 0..1 {
 
         // }
